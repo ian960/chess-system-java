d43b9b3beed8f02f8653b018825b0977e0c8791d
ian <ianjudo552@gmail.com>
2025-06-24 20:16:51 -0300

    clean screen and exceptions

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,7 +25,7 @@ public class ChessMatch {
         return mat;
     }
 
-    public ChessPiece peformChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition){
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
